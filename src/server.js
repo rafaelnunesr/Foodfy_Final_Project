daaed092274cf8db0f3378a04778ac423e82ad31
Rafael Nunes <rafaelrafssk@hotmail.com>
2020-08-17 10:46:02 -0300

    foodfy init

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const methodOverride = require('method-override')
+
+const routes = require('./routes')
+const session = require('./config/session')
+
+
+const server = express()
+
+server.use(session)
+server.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
+
+nunjucks.configure('src/app/views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.set('view engine', 'njk')
+
+server.listen(5000, function() {
+    console.log('server is running')
+})
